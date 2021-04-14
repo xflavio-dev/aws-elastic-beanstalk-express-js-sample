28932da330f297e94029977d1c61b17e8c7d0023
xflavio <flavio@flavioferreira.com.br>
2021-04-14 14:46:21 -0300

    Full pipeline test 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! xf4v10.local'));
+app.get('/', (req, res) => res.send('Hello World! xf4v10 123'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
